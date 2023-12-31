f4728df08a46339c7dac6738919c02064dd7566f
juan bernal <mesieou@gmail.com>
2023-12-31 12:27:47 +1100

    my first event listener exercise completed

--- 02-My-First-Event-Listener/lib/listener.js
+++ 02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,9 @@
 // TODO: React to a click on the button!
+const click = document.querySelector('#clickme')
+
+click.addEventListener('click', (event) => {
+  event.currentTarget.textContent = 'Bingo!';
+  event.currentTarget.classList.add('disabled');
+  let audio = new Audio ('../sound.mp3')
+  audio.play();
+})
