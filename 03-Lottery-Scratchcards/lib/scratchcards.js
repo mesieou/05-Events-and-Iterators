3f036aa0d38a65b8e1687cd5307a619ffc103067
juan bernal <mesieou@gmail.com>
2023-12-31 13:24:59 +1100

    cost of cards event listener added aand amount won

--- 03-Lottery-Scratchcards/lib/scratchcards.js
+++ 03-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,17 @@
 // TODO: Write your gambling application here.
+
+let cards = document.querySelectorAll('.scratchcard');
+let balanceElement = document.querySelector('#balance');
+let balance = parseInt(balanceElement.textContent, 10)
+
+cards.forEach((card) => {
+  card.addEventListener('click', (event) => {
+    let cardAmount = event.currentTarget.dataset.amount
+    if (balance.textContent == 0) {
+      alert('Game Over')
+    } else {
+      console.log(balanceElement.textContent)
+      balanceElement.textContent = balance + parseInt(cardAmount, 10) - 10;
+    }
+  });
+});
