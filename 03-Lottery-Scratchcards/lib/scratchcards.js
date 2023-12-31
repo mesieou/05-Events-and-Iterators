7ae9dcbd50e06cac1a95d956a242b289e5c1476d
juan bernal <mesieou@gmail.com>
2023-12-31 13:52:35 +1100

    displaying amount won feature added

--- 03-Lottery-Scratchcards/lib/scratchcards.js
+++ 03-Lottery-Scratchcards/lib/scratchcards.js
@@ -6,12 +6,19 @@ let balance = parseInt(balanceElement.textContent, 10)
 
 cards.forEach((card) => {
   card.addEventListener('click', (event) => {
-    let cardAmount = event.currentTarget.dataset.amount
+    let card = event.currentTarget
+    let cardAmount = card.dataset.amount
+
     if (balance.textContent == 0) {
       alert('Game Over')
     } else {
-      console.log(balanceElement.textContent)
+      card.dataset.scratched = "true";
       balanceElement.textContent = balance + parseInt(cardAmount, 10) - 10;
+      if (card.classList.contains('disabled')) {
+      } else {
+        card.insertAdjacentHTML('beforeEnd', `$${cardAmount}.00`)
+      };
+      card.classList.add('disabled');
     }
   });
 });
