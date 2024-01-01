28f78f84d12071b7925cbf561f2e602c5fae4ef0
juan bernal <mesieou@gmail.com>
2024-01-02 10:49:25 +1100

    added announce winner feature

--- 04-Wagon-Race/lib/wagon_race.js
+++ 04-Wagon-Race/lib/wagon_race.js
@@ -1,23 +1,52 @@
 // TODO: write your code here
-const playerOneClass = '#player1-race'
-const playertwoClass = '#player2-race'
+const playerOneClass = '#player1-race';
+const playertwoClass = '#player2-race';
 
+const getPosition = (playerClass) => {
+  let player = document.querySelector(`${playerClass} .active`);
+  return player.cellIndex;
+};
+
+const getPlayer = (playerClass) => {
+  return document.querySelector(`${playerClass} .active`);
+};
+
+const getTracks = (playerClass) => {
+  return document.querySelectorAll(`${playerClass} td`);
+};
+
+const announceWinner = (playerClass) => {
+  const tracks = getTracks(playerClass)
+  const position = getPosition(playerClass)
+
+  if (position == tracks.length - 1 ) {
+    if (playerClass == '#player1-race') {
+      alert('Player one wins!')
+      location.reload();
+    } else {
+      alert('Player two wins!')
+      location.reload();
+    };
+  };
+};
 
 const movePlayer = (playerClass) => {
-  const tracks = document.querySelectorAll(`${playerClass} td`);
-  let player = document.querySelector(`${playerClass} .active`);
-  let position = player.cellIndex
+  const tracks = getTracks(playerClass)
+  let position = getPosition(playerClass)
+  let player = getPlayer(playerClass)
 
-  player.classList.remove('active')
+  player.classList.remove('active');
 
-  position += 1
-  tracks[position].classList.add('active')
+  position += 1;
+  tracks[position].classList.add('active');
 };
 
 document.addEventListener('keydown', (event) => {
   if (event.key == 'q') {
-    movePlayer(playerOneClass)
+    movePlayer(playerOneClass);
+    announceWinner(playerOneClass);
   } if (event.key == 'p') {
-    movePlayer(playertwoClass)
+    movePlayer(playertwoClass);
+    announceWinner(playertwoClass)
   }
 });
