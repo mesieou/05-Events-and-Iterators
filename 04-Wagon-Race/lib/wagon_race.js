a4e2477721c50c6b371d8773ba2230bb3e1aa37f
juan bernal <mesieou@gmail.com>
2024-01-02 10:04:35 +1100

    event listener keydown added

--- 04-Wagon-Race/lib/wagon_race.js
+++ 04-Wagon-Race/lib/wagon_race.js
@@ -3,25 +3,21 @@ const playerOneClass = '#player1-race'
 const playertwoClass = '#player2-race'
 
 
-const playerOne = document.querySelector("#player1-race .active")
-const playerTwo = document.querySelector("#player2-race .active")
-let playerOnePosition = playerOne.cellIndex
-let playerTwoPosition = playerTwo.cellIndex
-
-
 const movePlayer = (playerClass) => {
-  const tracks = document.querySelectorAll(`${playerClass} td`)
-  let player = document.querySelector(`${playerClss} .active`)
+  const tracks = document.querySelectorAll(`${playerClass} td`);
+  let player = document.querySelector(`${playerClass} .active`);
+  let position = player.cellIndex
 
-  const newPosition = car.cellIndex + 1
+  player.classList.remove('active')
 
-  car.classList.remove('active')
-  tracks[newPosition].classList.add('active')
+  position += 1
+  tracks[position].classList.add('active')
 };
 
-
-
-// Add event listener for player one with keyup
-// add class to the next position
-
-// Add event listener for player two
+document.addEventListener('keydown', (event) => {
+  if (event.key == 'q') {
+    movePlayer(playerOneClass)
+  } if (event.key == 'p') {
+    movePlayer(playertwoClass)
+  }
+});
