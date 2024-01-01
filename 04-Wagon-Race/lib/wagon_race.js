5130e61eb2dc719cae3de345b0c4c1f1a088aa94
juan bernal <mesieou@gmail.com>
2024-01-01 17:27:17 +1100

    wagon race move player function completed

--- 04-Wagon-Race/lib/wagon_race.js
+++ 04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,27 @@
 // TODO: write your code here
+const playerOneClass = '#player1-race'
+const playertwoClass = '#player2-race'
+
+
+const playerOne = document.querySelector("#player1-race .active")
+const playerTwo = document.querySelector("#player2-race .active")
+let playerOnePosition = playerOne.cellIndex
+let playerTwoPosition = playerTwo.cellIndex
+
+
+const movePlayer = (playerClass) => {
+  const tracks = document.querySelectorAll(`${playerClass} td`)
+  let player = document.querySelector(`${playerClss} .active`)
+
+  const newPosition = car.cellIndex + 1
+
+  car.classList.remove('active')
+  tracks[newPosition].classList.add('active')
+};
+
+
+
+// Add event listener for player one with keyup
+// add class to the next position
+
+// Add event listener for player two
