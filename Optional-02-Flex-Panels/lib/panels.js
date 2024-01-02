350eee5e2745e76714c10a5bc81901eb1e88d9ea
juan bernal <mesieou@gmail.com>
2024-01-02 15:35:53 +1100

    added event listener to the panels to make them bigger

--- Optional-02-Flex-Panels/lib/panels.js
+++ Optional-02-Flex-Panels/lib/panels.js
@@ -1,1 +1,21 @@
 console.log('panels.js loaded');
+const panels = document.querySelectorAll('.panel')
+const panelsArray = Array.from(panels)
+
+panelsArray.forEach((panel) => {
+  panel.addEventListener('click', (event) => {
+    let hiddenTextElements = panel.querySelectorAll('p.hidden')
+
+    // Making Panels bigger by adding the toggling open/closed classes
+    panelsArray.forEach((otherPanel) => {
+      if (otherPanel !== event.currentTarget) {
+        otherPanel.classList.remove('open')
+      } else {
+        event.currentTarget.classList.toggle('open');
+      }
+    });
+
+    // Turning text back on
+    hiddenTextElements.forEach(elem => elem.classList.toggle('hidden'));
+  });
+});
